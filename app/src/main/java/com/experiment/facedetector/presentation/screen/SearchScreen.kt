a3f779eeffebddf21dd8ee21a86f191553d7d09a
jeishiva <s.sivaguru11@gmail.com>
2025-09-01 17:58:50 +0530

    unused imports removed

--- app/src/main/java/com/experiment/facedetector/presentation/screen/SearchScreen.kt
+++ app/src/main/java/com/experiment/facedetector/presentation/screen/SearchScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -329,7 +328,6 @@ private fun GridItemLoader() {
         )
     }
 }
-
 
 @Composable
 @Preview(showBackground = true)
